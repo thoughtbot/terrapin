c343bb712e0400d747fd16556e488a743bab5869
Leif Gensert <lgensert@triumphpay.com>
2025-02-28 08:35:20 -0800

    make options consistent

--- spec/terrapin/command_line/runners/backticks_runner_spec.rb
+++ spec/terrapin/command_line/runners/backticks_runner_spec.rb
@@ -2,7 +2,7 @@ require 'spec_helper'
 
 describe Terrapin::CommandLine::BackticksRunner do
   if Terrapin::CommandLine::BackticksRunner.supported?
-    it_behaves_like 'a command that does not block', :supports_stderr => false
+    it_behaves_like 'a command that does not block', { :supports_stderr => false }
 
     it 'runs the command given and captures the output in an Output' do
       output = subject.call("echo hello")
